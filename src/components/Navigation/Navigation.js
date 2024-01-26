4aa652d5563e1fffbfc4ccf0a4ac585a1da30442
Plamena Petkova <pl.petkova@gmail.com>
2024-01-26 14:42:57 +0200

    Removed buttons from nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,16 @@
 import "./Navigation.scss";
+import logo from "../../assets/LogoProfile.png";
 
 export function Navigation() {
   return (
     <nav className="container__nav">
-      <button className="container__nav--btn">Home</button>
+      <div className="container__nav--logo">
+        <img src={logo} alt="" className="container__nav--logo-img" />
+      </div>
+      <button className="container__nav--btn">Plamena Petkova</button>
+      <button className="container__nav--btn">Projects</button>
       <button className="container__nav--btn">Timestamp</button>
       <button className="container__nav--btn">Certificates</button>
-      <button className="container__nav--btn">TechStack</button>
-      <button className="container__nav--btn">Projects</button>
     </nav>
   );
 }
