dfd6ad8efa651e9f2bb77b0dfa43b7749b245c13
Plamena Petkova <pl.petkova@gmail.com>
2024-01-28 01:24:23 +0200

    Added favicon changes in projectItem component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,8 +8,8 @@ export function Navigation() {
         <img src={logo} alt="" className="container__nav--logo-img" />
       </div>
       <button className="container__nav--btn">Home</button>
-      <button className="container__nav--btn">Projects</button>
       <button className="container__nav--btn">Timeline</button>
+      <button className="container__nav--btn">Projects</button>
       <button className="container__nav--btn">Certificates</button>
     </nav>
   );
--- src/components/Projects/ProjectItem/ProjectItem.js
+++ src/components/Projects/ProjectItem/ProjectItem.js
@@ -1,17 +1,15 @@
 import "./ProjectItem.scss";
-import logoChat from "../../../assets/chatzakLogo.png";
+import logoChat from "../../../assets/remote-5491798_1280.png";
+import { Button } from "../../Button/Button";
 
 export function ProjectItem() {
   return (
     <div className="project__card">
-      <h2 className="project__card--heading">
-        ChatZak application to connect people
-      </h2>
-      <img
-        src={logoChat}
-        alt="project-1"
-        className="project__card--img"
-      />
+      <h2 className="project__card--heading">ChatZak</h2>
+      <div className="project__card--base">
+        <img src={logoChat} alt="chat-logo" className="project__card--img" />
+        <Button caption={"Show"} buttonClass={"btn btn--project"} />
+      </div>
     </div>
   );
 }
