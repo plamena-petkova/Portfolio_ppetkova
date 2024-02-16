135d84c6cf99fdbd88303e10eaa08cadb3def561
Plamena Petkova <pl.petkova@gmail.com>
2024-02-16 10:50:57 +0200

    Added observer and button to scroll to top when going down

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,3 +1,5 @@
+import { useInView } from "react-intersection-observer";
+import { Button } from "../components/Button/Button";
 import { Certificates } from "../components/Certificates/Certificates";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
@@ -6,13 +8,25 @@ import { ProjectView } from "../components/Projects/ProjectView/ProjectView";
 import { Skills } from "../components/Skills/Skills";
 import { TimelineView } from "../components/Timeline/TimelineView/TimelineView";
 
-
 export function HomeView() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  const { ref: aboutRef, inView: aboutSectionIsVisible } = useInView();
 
   return (
     <>
-      <Navigation />
-      <Header />
+      <div ref={aboutRef}>
+        {!aboutSectionIsVisible && <Navigation /> && (
+          <Button
+            caption={"Up"}
+            btnClicked={scrollToTop}
+            buttonClass={"btn btn--transparent btn--transparent-scroll"}
+          />
+        )}
+        <Header />
+      </div>
       <TimelineView />
       <ProjectView />
       <Skills />
