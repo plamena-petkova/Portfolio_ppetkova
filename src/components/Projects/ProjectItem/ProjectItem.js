385d7b417497b360854a6662d3bd0f4f538d2e51
Plamena Petkova <pl.petkova@gmail.com>
2025-03-13 15:15:47 +0200

    Added new styles in ProjectItem and ProjectView components

--- src/components/Projects/ProjectItem/ProjectItem.js
+++ src/components/Projects/ProjectItem/ProjectItem.js
@@ -2,37 +2,48 @@ import "./ProjectItem.scss";
 import { Button } from "../../Button/Button";
 import { Chip } from "../../Chip/Chip";
 
-export function ProjectItem({project}) {
-
+export function ProjectItem({ project }) {
   const techStackArray = Object.values(project.techStack);
-  const items = techStackArray[0].split(',');
+  const items = techStackArray[0].split(",");
 
   return (
     <article className="project__card">
       <div className="project__card--picture">
-        <img src={project.picture} alt="chat-logo" className="project__card--picture-img" />
+        <img
+          src={project.picture}
+          alt="chat-logo"
+          className="project__card--picture-img"
+        />
       </div>
       <div className="project__card--text">
         <h2 className="project__card--heading">{project.name}</h2>
         <div className="project__card--text-techstack">
-          {items.map((stack, index) => {
-            return <Chip key={index} techName={stack}/>
+          {items.map((stack) => {
+            return <Chip key={stack} techName={stack} />;
           })}
         </div>
-        <p>
-          {project.about}
-        </p>
+        <p>{project.about}</p>
         <div className="project__card-btns">
-         
-           <Button hrefLink={project.liveDemo} caption={`Live Demo`} buttonClass={"btn btn--project"} newTab={true} />
-       
-          {project.gitRepo.map((btn) => {
-             return <Button hrefLink={btn.link} caption={`GutHub Repo ${btn.use}`} buttonClass={"btn btn--project"} newTab={true} />
+          <Button
+            hrefLink={project.liveDemo}
+            caption={`Live Demo`}
+            buttonClass={"btn btn--project"}
+            newTab={true}
+          />
+
+          {project.gitRepo.map((btn, index) => {
+            return (
+              <Button
+                key={index}
+                hrefLink={btn.link}
+                caption={`GutHub Repo ${btn.use}`}
+                buttonClass={"btn btn--project"}
+                newTab={true}
+              />
+            );
           })}
-       
         </div>
       </div>
     </article>
   );
 }
-
--- src/components/Projects/ProjectView/ProjectView.js
+++ src/components/Projects/ProjectView/ProjectView.js
@@ -1,27 +1,21 @@
-import { useEffect, useState } from "react";
 import { useDeveloperContext } from "../../../context/developerContext";
 import { ProjectItem } from "../ProjectItem/ProjectItem";
 import "./ProjectView.scss";
 
 export function ProjectView() {
-
-  const {projects} = useDeveloperContext();
-
-  const [projectsArray, setProjectsArray] = useState([]);
-
-  useEffect(() => {
-    if(projects) {
-      setProjectsArray(projects.projects)
-    }
-  }, [projects])
-
+  const { projects } = useDeveloperContext();
 
   return (
     <section id="projects" className="container__projects">
-      <h2 className="container__projects-heading">Personal <span className="container__projects-heading--italic">Projects</span></h2>
-      {projectsArray?.length && projectsArray.map((project) => {
-        return <ProjectItem key={project._id} project={project} />
-      })}
+      <h2 className="container__projects-heading">
+        Personal{" "}
+        <span className="container__projects-heading--italic">Projects</span>
+      </h2>
+      <div className="container__projects--items">
+        {projects?.projects?.map((project) => (
+          <ProjectItem key={project._id} project={project} />
+        ))}
+      </div>
     </section>
   );
 }
