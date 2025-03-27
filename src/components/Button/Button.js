07c7e09313dea69e4f3c506de3823efb9e7422f3
Plamena Petkova <pl.petkova@gmail.com>
2025-03-27 18:06:41 +0200

    Added icon to scroll button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,18 +1,22 @@
 import { HashLink } from "react-router-hash-link";
 import "./Button.scss";
+import IcomoonReact from "icomoon-react";
+import iconSet from "../../assets/selection.json";
 
-export function Button({
-  caption,
-  buttonClass,
-  btnClicked,
-  hrefLink,
-  newTab
-}) {
-
+export function Button({ caption, buttonClass, btnClicked, hrefLink, newTab }) {
   return (
     <HashLink className="link" to={hrefLink} target={newTab ? "_blank" : null}>
       <button onClick={btnClicked} className={buttonClass}>
-        {caption}
+        {caption === "Up" ? (
+          <IcomoonReact
+            iconSet={iconSet}
+            color="#1e1e1e"
+            size={20}
+            icon={"arrow-up2"}
+          />
+        ) : (
+          caption
+        )}
       </button>
     </HashLink>
   );
